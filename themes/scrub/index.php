57f9e035da9ebc13e35480a85e33e0810fd06bd6
scrub <scrub.mx@gmail.com>
2013-06-11 23:57:27 -0500

    mobile meta tags - index.php

--- themes/scrub/index.php
+++ themes/scrub/index.php
@@ -5,11 +5,13 @@
 	<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
+		<title><?php bloginfo('name'); ?></title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<title><?php bloginfo('name'); ?></title>
+		<meta http-equiv="cleartype" content="on">
 
 		<?php wp_head(); ?>
 	</head>
